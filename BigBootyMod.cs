51f2c6ba68968464acfd1578241d813115eb459f
Smug B <43657791+Smug-B@users.noreply.github.com>
2024-01-23 17:35:25 -0500

    BigBootyMod Utility Members

--- BigBootyMod.cs
+++ BigBootyMod.cs
@@ -1,8 +1,16 @@
+using ReLogic.Content;
+using System.Diagnostics.CodeAnalysis;
 using Terraria.ModLoader;
 
 namespace BigBootyMod
 {
 	public class BigBootyMod : Mod
 	{
-	}
+        [NotNull]
+        public static BigBootyMod? Mod { get; private set; }
+
+        public BigBootyMod() => Mod = this;
+
+        public static Asset<T> Request<T>(string assetName, AssetRequestMode mode = AssetRequestMode.AsyncLoad) where T : class => Mod.Assets.Request<T>(assetName, mode);
+    }
 }
